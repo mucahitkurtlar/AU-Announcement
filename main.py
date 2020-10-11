347eab671cb2387e71378cfe9619b0ede556f588
Mucahit KURTLAR <mucahit@tutanota.com>
2020-10-11 16:38:47 +0300

    Add email sender

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from bs4 import BeautifulSoup as soup  # HTML data structure
 from urllib.request import urlopen as uReq  # Web client
+import smtplib
+from secrets import sender_email, receiver_email, password
+
+tr_chars = {'ç':'c', 'Ç':'C', 'ğ':'g', 'Ğ':'G', 'ı':'i', 'İ':'I', 'ö':'o', 'Ö':'O', 'ş':'s', 'Ş':'S', 'ü':'u', 'Ü':'U'}
 
 url_file = open("last_url.txt", "r")
 file_urls = []
@@ -43,6 +47,7 @@ if len(post_urls) != 0:
     url_file.write(post_urls[0])
     url_file.close()
 
+post_contents = []
 for post_url in post_urls:
     print(post_url)
     uClient = uReq(post_url)
@@ -50,3 +55,33 @@ for post_url in post_urls:
     uClient.close()
     post_containers = post_soup.findAll("div", {"class": "post-content"})
     print(post_containers[0].text)
+    post_contents.append(post_containers[0].text)
+
+
+sent_from = sender_email
+to = [receiver_email]
+subject = 'OMG Super Important Message'
+
+for src, target in tr_chars.items():
+    post_contents[0] = post_contents[0].replace(src, target)
+
+body = post_contents[0].encode('ascii', 'ignore').decode('ascii')
+
+email_text = """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (sent_from, ", ".join(to), subject, body)
+
+try:
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(sender_email, password)
+    server.sendmail(sent_from, to, email_text)
+    server.close()
+
+    print("Email sent!")
+except Exception as e: # work on python 2.x
+    print('Failed to send email: '+ str(e))
