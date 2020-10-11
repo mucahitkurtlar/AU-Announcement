ce649aa7317805676abc68238abd50b25ca5f1e2
Mucahit KURTLAR <mucahit@tutanota.com>
2020-10-11 17:12:34 +0300

    Add functions

--- main.py
+++ main.py
@@ -3,16 +3,52 @@ from urllib.request import urlopen as uReq  # Web client
 import smtplib
 from secrets import sender_email, receiver_email, password
 
-tr_chars = {'ç':'c', 'Ç':'C', 'ğ':'g', 'Ğ':'G', 'ı':'i', 'İ':'I', 'ö':'o', 'Ö':'O', 'ş':'s', 'Ş':'S', 'ü':'u', 'Ü':'U'}
+def tr2ascii(raw_string):
+    string = raw_string
+    tr_chars = {'ç':'c', 'Ç':'C', 'ğ':'g', 'Ğ':'G', 'ı':'i', 'İ':'I', 'ö':'o', 'Ö':'O', 'ş':'s', 'Ş':'S', 'ü':'u', 'Ü':'U'}
+    for src, target in tr_chars.items():
+        string = string.replace(src, target)
+    return string
+
+def send_email(msg):
+    sent_from = sender_email
+    to = [receiver_email]
+    subject = 'New Announcement | Yeni Duyuru'
+    body_raw = tr2ascii(msg)
+    body = body_raw.encode('ascii', 'ignore').decode('ascii')
+    email_text = """\
+    From: %s
+    To: %s
+    Subject: %s
+
+    %s
+    """ % (sent_from, ", ".join(to), subject, body)
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(sender_email, password)
+        server.sendmail(sent_from, to, email_text)
+        server.close()
+
+        print("Email sent!")
+    except Exception as e:
+        print('Failed to send email: '+ str(e))
+
+def list2str(str_list):
+    string = ""   
+    for elem in str_list:  
+        string += elem + "----------------------------------------------------------"
+    return string
+        
+
 
 url_file = open("last_url.txt", "r")
 file_urls = []
 for file_url in url_file:
-    print(file_url)
+    print("The last url: " + file_url)
     file_urls.append(file_url)
 url_file.close()
-
-print(file_urls[0])
 
 i = 1
 post_urls = []
@@ -29,15 +65,12 @@ while next:
     for container in containers:
         container_url = container.a["href"]
         if len(file_urls) == 0:
-            print("if")
             print(container_url)
             post_urls.append(container_url)
         elif not(container_url in file_urls[0]):
-            print("elif")
             print(container_url)
             post_urls.append(container_url)
         else:
-            print("else")
             next = False
             break
 
@@ -57,31 +90,8 @@ for post_url in post_urls:
     print(post_containers[0].text)
     post_contents.append(post_containers[0].text)
 
-
-sent_from = sender_email
-to = [receiver_email]
-subject = 'OMG Super Important Message'
-
-for src, target in tr_chars.items():
-    post_contents[0] = post_contents[0].replace(src, target)
-
-body = post_contents[0].encode('ascii', 'ignore').decode('ascii')
-
-email_text = """\
-From: %s
-To: %s
-Subject: %s
-
-%s
-""" % (sent_from, ", ".join(to), subject, body)
-
-try:
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.ehlo()
-    server.login(sender_email, password)
-    server.sendmail(sent_from, to, email_text)
-    server.close()
-
-    print("Email sent!")
-except Exception as e: # work on python 2.x
-    print('Failed to send email: '+ str(e))
+if len(post_contents) > 0:
+    msg = list2str(post_contents)
+    send_email(msg)
+else:
+    print("Nothing new!")
